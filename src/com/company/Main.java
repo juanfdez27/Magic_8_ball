8c7c8e0b53bbd818e890cee04ae5ff6aea3ec80c
juan <juan.fernandez.doremire.2020@student.lhschools.org>
2019-02-14 08:09:15 -0500

    declared variables

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// declare
+
+        Scanner keyboard;
+        int testing;
+        String question;
+        
+
+
+
+
+
+
+
+    }
+}
